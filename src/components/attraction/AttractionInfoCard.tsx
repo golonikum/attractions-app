cc91b0f231034ccdb7ae87b040acca14c56105af
Голов Никита Викторович <n.golov@zetra.pro>
2026-02-02 16:35:24 +0300

    Fix

--- src/components/attraction/AttractionInfoCard.tsx
+++ src/components/attraction/AttractionInfoCard.tsx
@@ -30,7 +30,7 @@ export function AttractionInfoCard({ attraction }: AttractionInfoCardProps) {
       >
         <AttractionImage
           attraction={attraction}
-          className={`rounded-md ${isWideScreen ? "h-full w-48" : "h-48 w-full"}`}
+          className={`rounded-md ${isWideScreen ? "h-full w-96" : "h-96 w-full"}`}
         />
 
         <div className="flex flex-col gap-4">
