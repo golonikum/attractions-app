76db28929c38a5088b45a31cf362217d6a2dafdb
Голов Никита Викторович <n.golov@zetra.pro>
2026-02-05 17:02:59 +0300

    Fix

--- src/components/ui/Map.tsx
+++ src/components/ui/Map.tsx
@@ -79,8 +79,8 @@ export const Map: FC<MapPropsType> = ({
       </YMapControls>
       {attractions?.map((attraction) => {
         const isActive =
-          location.center[0] === attraction.coordinates[1] &&
-          location.center[1] === attraction.coordinates[0];
+          Math.abs(location.center[0] - attraction.coordinates[1]) < 0.00001 &&
+          Math.abs(location.center[1] - attraction.coordinates[0]) < 0.00001;
 
         console.log("isActive", location, attraction.coordinates);
 
