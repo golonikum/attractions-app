b8150b8c658e8b49dfc223cebfdb9731bba0b042
Голов Никита Викторович <n.golov@zetra.pro>
2026-02-26 15:08:39 +0300

    Fix

--- src/lib/locateItemOnMainMap.ts
+++ src/lib/locateItemOnMainMap.ts
@@ -1,7 +1,7 @@
 import { Attraction } from "@/types/attraction";
 import { Group } from "@/types/group";
 import { isAttraction } from "./utils";
-import { DEFAULT_ATTRACTION_ZOOM, DEFAULT_GROUP_ZOOM } from "./constants";
+import { DEFAULT_ATTRACTION_ZOOM } from "./constants";
 
 export const locateItemOnMainMap = ({
   router,
@@ -12,7 +12,7 @@ export const locateItemOnMainMap = ({
 }) => {
   const isGroup = !isAttraction(item);
 
-  const newUrl = `${window.location.pathname}?zoom=${isGroup ? DEFAULT_GROUP_ZOOM : DEFAULT_ATTRACTION_ZOOM}&coordinates=${item.coordinates[1]}%2C${item.coordinates[0]}`;
+  const newUrl = `${window.location.pathname}?zoom=${isGroup ? item.zoom : DEFAULT_ATTRACTION_ZOOM}&coordinates=${item.coordinates[1]}%2C${item.coordinates[0]}`;
   window.history.pushState({}, "", newUrl);
 
   router.push("/main");
