868b32f7cd0ecad3bf3c6217fcb4c9df78d47680
Голов Никита Викторович <n.golov@zetra.pro>
2026-03-20 13:57:31 +0300

    Fix

--- src/app/api/import/route.ts
+++ src/app/api/import/route.ts
@@ -4,15 +4,16 @@ import { withAuth } from '@/lib/serverAuth';
 
 export async function GET(request: NextRequest) {
   return withAuth(request, async () => {
-    const response = await fetch(
-      `https://geocode-maps.yandex.ru/v1?apikey=${process.env.YA_MAPS_API_KEY}&lang=ru_RU&geocode=достопримечательности%20Саратова&format=json&results=15`,
-      {
-        method: 'GET',
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-        },
+    const url = `https://geocode-maps.yandex.ru/v1?apikey=${process.env.YA_MAPS_API_KEY}&lang=ru_RU&geocode=достопримечательности%20Саратова&format=json&results=15`;
+
+    console.log(url);
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
       },
-    );
+    });
     const data = await response.json();
 
     return NextResponse.json(
