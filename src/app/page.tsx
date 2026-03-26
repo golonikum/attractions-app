61ce87af7156ad38f8557659e1a3078470e9678a
Голов Никита Викторович <n.golov@zetra.pro>
2026-03-26 16:53:57 +0300

    Refactor app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-import { Loader } from '@/components/ui/Loader';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/main');
-  }, [router]);
-
-  return <Loader />;
+  redirect('/main');
 }
