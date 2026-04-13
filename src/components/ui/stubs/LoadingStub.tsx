7ef46f0f53a484fbff7985f962e955ea5af52c06
Голов Никита Викторович <n.golov@zetra.pro>
2026-04-13 19:37:25 +0300

    Loading stub

--- src/components/ui/stubs/LoadingStub.tsx
+++ src/components/ui/stubs/LoadingStub.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 export const LoadingStub = () => (
   <div className="container mx-auto pt-20 px-4 pb-8">
     <div className="flex justify-center items-center h-64">
-      <Loader2 className="h-8 w-8 animate-spin" />
+      <Loader2 className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
     </div>
   </div>
 );
