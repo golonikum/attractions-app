3f63ff2b7766d90bc350955474b18f44a1bdaa8d
Голов Никита Викторович <n.golov@zetra.pro>
2026-04-18 11:02:09 +0300

    Fix

--- src/components/ui/table/Table.tsx
+++ src/components/ui/table/Table.tsx
@@ -12,7 +12,7 @@ export const Table: FC<PropsWithChildren<{ columns: TableColumnConfig[]; classNa
   className,
 }) => (
   <div className={cn('h-full overflow-auto flex flex-col width-fit', className)}>
-    <div className="bg-gray-50 h-8 flex sticky top-0 z-10">
+    <div className="bg-gray-50 h-8 flex sticky top-0 z-10 border-b-1 border-gray-200">
       {columns.map((column) => (
         <TableHeaderCell column={column} key={column.label} />
       ))}
