33e03de12bb1f462a1d894cd32a4e0dda282a8ad
Голов Никита Викторович <n.golov@zetra.pro>
2026-04-18 11:26:24 +0300

    Big image map pin on map

--- src/components/ui/MarkerPin.tsx
+++ src/components/ui/MarkerPin.tsx
@@ -1,13 +1,15 @@
 import { FC, useState } from 'react';
 import { MapPin } from 'lucide-react';
 
+import { useIsMobile } from '@/hooks/useIsMobile';
+import { cn } from '@/lib/utils';
 import { YMapMarker } from '@/lib/ymaps';
 
 const getMapPinClassNames = (isActive: boolean, visited: boolean) => {
   if (isActive) {
     return 'text-blue-500 fill-blue-200';
   } else if (visited) {
-    return 'text-muted-foreground text-green-500 fill-green-200';
+    return 'text-muted-foreground fill-green-100';
   } else {
     return 'text-red-500 fill-red-200';
   }
@@ -22,6 +24,7 @@ export const MarkerPin: FC<{
 }> = ({ title, onClick, coordinates, visited = false, isActive = false }) => {
   const [isHovered, setIsHovered] = useState(false);
   const classNames = getMapPinClassNames(isActive, visited);
+  const { isMobile } = useIsMobile();
 
   return (
     <YMapMarker coordinates={coordinates} draggable={false} zIndex={isHovered || isActive ? 1 : 0}>
@@ -29,7 +32,11 @@ export const MarkerPin: FC<{
         <MapPin
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
-          className={`h-8 w-8 shrink-0 relative hover:text-blue-500 hover:fill-blue-200 cursor-pointer left-[-16px] top-[-32px] ${classNames}`}
+          className={cn(
+            isMobile ? 'h-12 w-12 left-[-24px] top-[-48px]' : 'h-8 w-8 left-[-16px] top-[-32px]',
+            'shrink-0 relative hover:text-blue-500 hover:fill-blue-200 cursor-pointer',
+            classNames,
+          )}
           onClick={onClick}
         />
         {(isHovered || isActive) && title && (
